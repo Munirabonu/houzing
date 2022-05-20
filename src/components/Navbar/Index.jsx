9d71b166d0037f92e64ba01dab5450cecbff148c
Munirabonu <muniratillaxojayeva@gmail.com>
2022-05-20 11:19:11 +0500

    first commit:

---
+++ src/components/Navbar/Index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Logo from "../../assets/logo.png";
+import "./style.css";
+
+
+export default function Navbar(params) {
+    return (
+        <>
+        <div className="Navbar">
+                <div className="container">
+                    <img src={Logo} alt="Logo" />
+                    <ul className="links">
+                        <li>
+                            <a>Home</a>
+                        </li>
+                        <li>
+                            <a>Properties</a>
+                        </li>
+                        <li>
+                            <a>Contacts</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "../components/Navbar/Index";
+
+export default function Home(params) {
+    return (
+        <div>
+           <Navbar /> 
+        </div>
+    )
+}
